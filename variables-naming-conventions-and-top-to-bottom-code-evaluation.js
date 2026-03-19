6ad1fd1e638d7762cbad90eeda9d051f27069944
Amanda Arseneau <aarseneau@outlook.com>
2026-03-19 09:37:57 -0500

    Updated for Recommit

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,84 @@ Things to reflect on:
   
 */
 
+/*
 let a = "Alice";
 let b = 5;
 let c = 20;
 let d = a + " bought " + b + " items for $" + c + ".";
 
 console.log(d);
+
+let a = "Alice";
+let b = 5;
+let c = 20;
+let d = a + " bought " + b + " items for $" + c + ".";
+
+console.log(d);
+
+/* 
+The first thing I noticed is that a,b,c,and d should all have descriptive names - so it would look something like this: 
+
+let shopperName = "Alice";
+let numofItems = 5;
+let dollarsSpent = 20;
+let receiptMsg = shopperName + " bought " + numofItems + " items for $" + dollarsSpent + ".";
+
+console.log(d);
+
+// Now I would attempt to re-write the purchase receipt message (receiptMsg) so it doesn't look so cluttered - something like this - and make sure I am following the camelCase rule: 
+
+let shopperName = "Alice";
+let numofItems = 5;
+let dollarsSpent = 20;
+let receiptMsg = '${shopperName} bought ${numofItems} items for $${dollarsSpent}.';
+
+console.log(receiptMsg);
+
+// Last I will add at least two additional variables, and update existing ones to be even more specific:
+
+let shopperFirstName = "Alice";
+let shopperLastName = "Cooper";
+let numofItems = 5;
+let itemName = "lottery ticket";
+let itemCost = 4;
+let totalDollarsSpent = 20;
+let receiptMsg = '${shopperFirstName} ${shopperLastName} bought ${numofItems} ${itemName}s at $${itemCost} a piece for $${dollarsSpent} total.';
+
+console.log(receiptMsg);
+let a = "Alice";
+let b = 5;
+let c = 20;
+let d = a + " bought " + b + " items for $" + c + ".";
+
+console.log(d);
+
+// The first thing I noticed is that a,b,c,and d should all have descriptive names - so it would look something like this: 
+
+let shopperName = "Alice";
+let numofItems = 5;
+let dollarsSpent = 20;
+let receiptMsg = shopperName + " bought " + numofItems + " items for $" + dollarsSpent + ".";
+
+console.log(d);
+
+// Now I would attempt to re-write the purchase receipt message (receiptMsg) so it doesn't look so cluttered - something like this - and make sure I am following the camelCase rule: 
+
+let shopperName = "Alice";
+let numofItems = 5;
+let dollarsSpent = 20;
+let receiptMsg = '${shopperName} bought ${numofItems} items for $${dollarsSpent}.';
+
+console.log(receiptMsg);
+
+// Last I will add at least two additional variables, and update existing ones to be even more specific:
+*/
+let shopperFirstName = "Alice";
+let shopperLastName = "Wonderland";
+let numofItems = 5;
+let itemName = "lottery ticket";
+let itemCost = 4;
+let totalDollarsSpent = 20;
+let receiptMsg = `${shopperFirstName} ${shopperLastName} bought ${numofItems} ${itemName}s at $${itemCost} a piece for $${totalDollarsSpent} total.`;
+
+console.log(receiptMsg);
